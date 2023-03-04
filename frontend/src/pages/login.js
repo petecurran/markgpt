c4626e05a5d69461e2b0890710a075a32d5e7c36
Pete Curran <Curranpete@gmail.com>
2023-03-04 13:28:08 +0000

    Starting mobile CSS for login page

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -26,31 +26,58 @@ const Login = (props) => {
     }
 
     return(
-        <div className="d-flex h-100 login-page">
-            <div className="container mx-auto my-auto login-card">
+        <div>
 
-                <div className="row h-100">
-                    <div className="col-md-4 login-column">
-                        <div className="login-text">
-                            <h1 className="text-center brand-text">markGPT</h1>
-                            <form>
-                                <div className="form-group">
-                                    <input type="text" className="form-control mt-3" id="username" placeholder="Enter username" onChange={(e)=> setUsername(e.target.value)} />
-                                    <input type="password" className="form-control mt-2" id="password" placeholder="Enter password" onChange={(e)=> setPassword(e.target.value)} />
-                                </div>
-                                <button type="submit" className="btn btn-primary mt-3 w-100 login-button" onClick={handleClick}>Login</button>
-                            </form>
+            {/* Mobile version */}
+            <div className="d-flex h-100 login-page-mobile d-md-none p-2">
+                <div className="container mx-auto my-auto login-card-mobile">
+                    <div className="row">
+                        <div className="col-12 login-picture-holder-mobile">
+                            <img src={roboteacher} alt="robot teacher" className="login-picture-mobile" />
                         </div>
                     </div>
-
-                    <div className="col-md-8 login-picture-holder">
-                        <img src={roboteacher} alt="robot teacher" className="login-picture" />
-                        
-
-
+                    <div className="row">
+                        <div className="col-12 login-column-mobile">
+                            <div className="login-text">
+                                <h1 className="text-center brand-text">markGPT</h1>
+                                <form>
+                                    <div className="form-group">
+                                        <input type="text" className="form-control mt-3" id="username" placeholder="Enter username" onChange={(e)=> setUsername(e.target.value)} />
+                                        <input type="password" className="form-control mt-2" id="password" placeholder="Enter password" onChange={(e)=> setPassword(e.target.value)} />
+                                    </div>
+                                    <button type="submit" className="btn btn-primary mt-3 w-100 login-button" onClick={handleClick}>Login</button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+
+
+            {/* Desktop version */}
+            <div className="d-flex h-100 login-page d-none d-md-flex">
+                <div className="container mx-auto my-auto login-card">
+                    <div className="row h-100">
+                        <div className="col-md-4 login-column">
+                            <div className="login-text">
+                                <h1 className="text-center brand-text">markGPT</h1>
+                                <form>
+                                    <div className="form-group">
+                                        <input type="text" className="form-control mt-3" id="username" placeholder="Enter username" onChange={(e)=> setUsername(e.target.value)} />
+                                        <input type="password" className="form-control mt-2" id="password" placeholder="Enter password" onChange={(e)=> setPassword(e.target.value)} />
+                                    </div>
+                                    <button type="submit" className="btn btn-primary mt-3 w-100 login-button" onClick={handleClick}>Login</button>
+                                </form>
+                            </div>
+                        </div>
+
+                        <div className="col-md-8 login-picture-holder">
+                            <img src={roboteacher} alt="robot teacher" className="login-picture" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
